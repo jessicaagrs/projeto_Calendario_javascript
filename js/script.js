4a7a6cdd4d4dc6d72c3d5c54f84c61e418722383
Jessica Aguiar <jessica.aguiar@brgaap.com>
2023-01-16 11:48:01 -0300

    commit inicial do projeto

---
+++ js/script.js
@@ -0,0 +1,39 @@
+let dateBegin = document.getElementById("input-dateBegin")
+let dateEnd = document.getElementById("input-dateEnd")
+let button = document.getElementById("btn-submit")
+let buttonClear = document.getElementById("btn-clean")
+let daysArray = document.getElementsByTagName("span")
+
+button.addEventListener("click", function () {
+    let dataIn = dateBegin.value
+    let day = Number(dataIn.slice(-2))
+    let dataFn = dateEnd.value
+    let day1 = Number(dataFn.slice(-2))
+
+    for (const key in daysArray) {
+        if (Number(key) == (day - 1)) {
+            daysArray[key].parentElement.className = "pontasData"
+        };
+    }
+
+    for (const key in daysArray) {
+        if (Number(key) == (day1 - 1)) {
+            daysArray[key].parentElement.className = "pontasData"
+        };
+    }
+
+    for (let i = day; i < (day1 - 1); i++) {
+        daysArray[i].classList = "entreData"
+    }
+})
+
+function clear() {
+    document.location.reload(true);
+}
+
+buttonClear.addEventListener("click", clear)
+
+
+
+
+
