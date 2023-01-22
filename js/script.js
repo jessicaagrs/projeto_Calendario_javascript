318e406643acdd6a82ad182da9ccf6b16a8c8717
Jessica Aguiar <88053349+jessicaagrs@users.noreply.github.com>
2023-01-22 15:07:26 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,33 +4,26 @@ let button = document.getElementById("btn-submit")
 let buttonClear = document.getElementById("btn-clean")
 let daysArray = document.getElementsByTagName("span")
 
-button.addEventListener("click", function () {
-    let dataIn = dateBegin.value
-    let day = Number(dataIn.slice(-2))
-    let dataFn = dateEnd.value
-    let day1 = Number(dataFn.slice(-2))
+button.addEventListener("click", () => {
+    const dataIn = dateBegin.value;
+    const startDay = Number(dataIn.slice(-2));
+    const dataFn = dateEnd.value;
+    const endDay = Number(dataFn.slice(-2));
     
-    if(dataIn[6] != dataFn[6] || dataIn[7] != dataFn[7]){
+    if(dataIn.slice(-2) !== dataFn.slice(-2)){
         alert("As datas precisam estar no mesmo mes")
-        clear()
+        clear();
+        return;
     }
 
-    for (const key in daysArray) {
-        if (Number(key) == (day - 1)) {
-            daysArray[key].parentElement.className = "pontasData"
-        };
+    for (let i = startDay - 1; i <= endDay - 1; i++) {
+        if (i === startDay - 1 || i === endDay - 1) {
+            daysArray[i].parentElement.className = "pontasData";
+        } else {
+            daysArray[i].classList = "entreData";
+        }
     }
-
-    for (const key in daysArray) {
-        if (Number(key) == (day1 - 1)) {
-            daysArray[key].parentElement.className = "pontasData"
-        };
-    }
-
-    for (let i = day; i < (day1 - 1); i++) {
-        daysArray[i].classList = "entreData"
-    }
-})
+});
 
 function clear() {
     document.location.reload(true);
